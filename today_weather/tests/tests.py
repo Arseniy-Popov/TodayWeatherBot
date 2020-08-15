98362ad619ee7b0d0839bd09b40f018301d40b9b
Arseniy Popov <arseniypopov@gmail.com>
2020-08-15 07:59:42 +0000

    -

--- today_weather/tests/tests.py
+++ today_weather/tests/tests.py
@@ -19,25 +19,34 @@ class TestTodayWeather(unittest.TestCase):
             api_hash=TELEGRAM_APP_API_HASH
         )
         cls.app.start()
+        @cls.app.on_message()
+        def register_response(client, message):
+            cls.response = message
 
     @classmethod
     def tearDownClass(cls):
         cls.app.stop()
     
     def setUp(self):
-        self.response = Empty
-        @self.app.on_message()
-        def register_response(client, message):
-            self.response = message
+        self.__class__.response = Empty
+    
+    # def tearDown(self):
+    #     self.app.remove_handler
     
     def _await_response(self):
-        while self.response == Empty:
+        while self.__class__.response == Empty:
             pass
     
     def test_welcome(self):
         self.app.send_message(self.bot, "/start")
         self._await_response()
         self.assertEqual(self.response.text, CONFIG["MESSAGES"]["WELCOME"])        
+
+    def test_address(self):
+        self.app.send_message(self.bot, "New York")
+        self._await_response()
+        self.assertEqual("\u00B0C" in self.response.text, True)
+        self.assertEqual("New York" in self.response.text, True)
 
 
 if __name__ == "__main__":
