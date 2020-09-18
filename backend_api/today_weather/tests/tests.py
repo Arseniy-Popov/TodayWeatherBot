dda37efec23f9d52143da1ce74579eef414667e0
Arseniy Popov <arseniypopov@gmail.com>
2020-09-18 18:47:47 +0000

    -

--- backend_api/today_weather/tests/tests.py
+++ backend_api/today_weather/tests/tests.py
@@ -4,14 +4,44 @@ import pytest
 from today_weather import app
 
 
+FORECAST_KEYS = ["rain", "snow", "temp_min", "temp_max"]
+LOCALITY_KEYS = ["lat", "lng", "name", "links"]
+
+
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
-    with app.test_client() as client:
-        yield client
+    # with app.test_client() as client:
+    #     yield client
+    yield app.test_client()
+
+def assert_keys_match(obj, keys):
+    assert len(obj) == len(keys)
+    for key in keys:
+        assert key in obj
 
 
-def test_post_address(client):
+def test_post_address(client, address="москва"):
+    response = client.post("/localities", json={"address": address})
+    assert response.status_code == 201
+    response = response.get_json()
+    assert_keys_match(response["forecast"], FORECAST_KEYS)
+    assert_keys_match(response["locality"], LOCALITY_KEYS)
+    assert "Moscow" in response["locality"]["name"]
+
+
+def test_post_address_cached(client):
+    test_post_address(client)
     response = client.post("/localities", json={"address": "москва"})
-    breakpoint
-    
+    assert response.status_code == 201
+
+
+@pytest.mark.parametrize("url, locality", [("/localities/1", "Moscow"), ("/localities/2", "New York")])
+def test_get_address(client, url, locality):
+    test_post_address(client)
+    test_post_address(client, address="new york")
+    response = client.get(url)
+    assert response.status_code == 200
+    response = response.get_json()
+    assert_keys_match(response["locality"], LOCALITY_KEYS)
+    assert locality in response["locality"]["name"]
