1c5b637c77435039da4b8bf7276eaf02296a7187
Arseniy Popov <arseniypopov@gmail.com>
2020-09-30 22:40:39 +0000

    -

--- backend_api/today_weather/tests/tests_deployed.py
+++ backend_api/today_weather/tests/tests_deployed.py
@@ -3,6 +3,7 @@ Test the deployed app.
 """
 
 
+import pytest
 import requests
 
 from today_weather.config import CONFIG
@@ -26,9 +27,24 @@ def test_get_locality_forecast():
     assert_keys_match(response["locality"], LOCALITY_KEYS)
 
 
-def test_post_address():
-    response = requests.post(URL + "/localities", json={"address": "moscow"})
+@pytest.mark.parametrize(
+    "address",
+    [
+        "Moscow",
+        "Berlin",
+        "Warsaw",
+        "Berlin",
+        "Amsterdam",
+        "Paris",
+        "London",
+        "Lisbon",
+        "Toronto",
+    ],
+)
+def test_post_address(address):
+    response = requests.post(URL + "/localities", json={"address": address})
+    assert response.status_code in (200, 201)
     response = response.json()
     assert_keys_match(response["forecast"], FORECAST_KEYS)
     assert_keys_match(response["locality"], LOCALITY_KEYS)
-    assert "Moscow" in response["locality"]["name"]
+    assert address in response["locality"]["name"]
